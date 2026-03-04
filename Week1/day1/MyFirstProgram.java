0579aed36b05c3b2947e14b468b768cd10145882
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-03-04 13:43:01 +0530

    Add files via upload

---
+++ Week1/day1/MyFirstProgram.java
@@ -0,0 +1,19 @@
+package week1.day1; 
+// camel lower casing pattern --> -> every 1st words 1st letter should start with the lowercase
+// weekOne --> other than 1st word , every words 1st letter should start with uppercase
+
+public class MyFirstProgram { //PASCAL CASING -> every words 1st letter should start with the uppercase
+    public static void main(String[] args) { 
+        
+
+
+
+
+
+
+        System.out.println("Hello");
+        System.out.print("World");
+    }
+}
+
+
